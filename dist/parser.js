dc985782e47be04920222bd302cc93fa57c21641
Leonardo Girotto <52132993+Leonardogrtt@users.noreply.github.com>
2021-10-25 13:31:25 -0300

    update

--- dist/parser.js
+++ dist/parser.js
@@ -1,31 +1,28 @@
 function toGeoJSON(doc) {
     const project = getProject(doc);
 
-    const canvasContainer = project["canvasContainer"];
-    const geoContainer = project["geometryContainer"];
-    const propContainer = project["propertyContainer"];
+    const canvasContainer = project.canvasContainer;
+    const geoContainer = project.geometryContainer;
+    const propContainer = project.propertyContainer;
 
-    const height = canvasContainer["F1"]["height"];
-    const width = canvasContainer["F1"]["width"];
+    const height = canvasContainer.F1.height;
+    const width = canvasContainer.F1.width;
 
-    const cells = geoContainer["cellGeometry"];
-    const states = geoContainer["stateGeometry"];
-    const transitions = geoContainer["transitionGeometry"];
+    const cells = geoContainer.cellGeometry;
+    const states = geoContainer.stateGeometry;
+    const transitions = geoContainer.transitionGeometry;
 
-    const polygons = getPolygons(cells, propContainer);
-    const points = getPoints(states, propContainer);
-    const lineStrings = getLineStrings(transitions, propContainer);
+    const cellProps = propertyContainer.cellProperties;
+    const stateProps = propertyContainer.stateProperties
+    const transitionProps = propertyContainer.transitionProperties;
+
+    const polygons = getPolygons(cells, cellProps, height);
+    const points = getPoints(states, stateProps, height);
+    const lineStrings = getLineStrings(transitions, transitionProps, height);
 
     const result = {
-        canvas: {
-            height,
-            width,
-        },
-        geo: {
-            polygons,
-            points,
-            lineStrings,
-        },
+        canvas: { height, width },
+        geo: { polygons, points, lineStrings }
     };
 
     return result;
@@ -38,65 +35,53 @@ function toGeoJSON(doc) {
         }
         return undefined;
     }
-    function getPolygons(cells, propertyContainer) {
-        if (cells.length > 0) {
-            let polis = [];
-            for (var i in cells) {
-                let property = propertyContainer["cellProperties"][i];
-                let poli = {
-                    geometry: {
-                        type: "Polygon",
-                        coordinates: [[]],
-                    },
-                    properties: { name: property.name },
-                };
-                for (let p of cells[i].points) {
-                    poli.geometry.coordinates[0].push([p.point.x, height - p.point.y]);
-                }
-                polis.push(poli);
-            }
-            return polis;
+    function getPolygons(cells, cellProps, height) {
+        if (cells.length < 1) return [];
+        return cells.map((c, idx) => getPolygon(c, cellProps[idx].name, height));
+    }
+    function getPoints(states, stateProps, height) {
+        if (states.length < 1) return [];
+        return states.map((s, idx) => getPoint(s, stateProps[idx].name, height));
+    }
+    function getLineStrings(transitions, transitionProps, height) {
+        if (transitions.length < 1) return [];
+        return transitions.map((t, idx) => getLineString(t, transitionProps[idx].name, height));
+    }
+    function getPolygon(cell, name, height) {
+        const coordinates = [
+            cell.points.map(p => [p.point.x, Number(height - p.point.y)])
+        ];
+        return {
+            geometry: getPolygonGeoJSON(coordinates),
+            properties: { name },
+        };
+    }
+    function getPoint(state, name, height) {
+        const coordinates = [
+            state.point.point.x, Number(height - state.point.point.y)
+        ];
+        return {
+            geometry: getPointGeoJSON(coordinates),
+            properties: { name }
         }
     }
-    function getPoints(states, propertyContainer) {
-        if (states.length > 0) {
-            let pts = [];
-            for (var i in states) {
-                let property = propertyContainer["stateProperties"][i];
-                let pt = {
-                    geometry: {
-                        type: "Point",
-                        coordinates: [
-                            states[i].point.point.x,
-                            height - states[i].point.point.y,
-                        ],
-                    },
-                    properties: { name: property.name },
-                };
-                pts.push(pt);
-            }
-            return pts;
+    function getLineString(transition, name, height) {
+        const coordinates = [
+            transition.points.map(p => [p.point.x, Number(height - p.point.y)])
+        ];
+        return {
+            geometry: getLineStringGeoJSON(coordinates),
+            properties: { name },
         }
     }
-    function getLineStrings(transitions, propertyContainer) {
-        if (transitions.length > 0) {
-            let lines = [];
-            for (var i in transitions) {
-                let property = propertyContainer["transitionProperties"][i];
-                let line = {
-                    geometry: {
-                        type: "LineString",
-                        coordinates: [],
-                    },
-                    properties: { name: property.name },
-                };
-                for (let p of transitions[i].points) {
-                    line.geometry.coordinates.push([p.point.x, height - p.point.y]);
-                }
-                lines.push(line);
-            }
-            return lines;
-        }
+    function getPolygonGeoJSON(coordinates) {
+        return { type: "Polygon", coordinates };
+    }
+    function getPointGeoJSON(coordinates) {
+        return { type: "Point", coordinates };
+    }
+    function getLineStringGeoJSON(coordinates) {
+        return { type: "LineString", coordinates };
     }
 }
 
